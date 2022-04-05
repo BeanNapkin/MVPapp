34341d910957ed5bff1332ad261fb14a86c09e0d
Margarita Fateeva <margarita@fateev.pro>
2022-04-06 01:00:53 +0400

    Extract strings to resources

--- app/src/main/java/pro/fateeva/mvpapp/Response.kt
+++ app/src/main/java/pro/fateeva/mvpapp/Response.kt
@@ -1,11 +1,12 @@
 package pro.fateeva.mvpapp
 
-enum class Response(val response: String) {
-    UNREGISTERED("Вы не зарегистированы"),
-    USERS_DUPLICATION("Пользователь с таким именем уже существует"),
-    EMPTY_CELLS("Заполните все поля"),
-    EMPTY_LOGIN("Введите свой логин"),
-    REGISTER_SUCCESS("Вы успешно зарегистрированы"),
-    CORRECT_PASSWORD("Верный пароль"),
-    UNCORRECT_PASSWORD("Неверный пароль")
+enum class Response(val response: Int) {
+    UNREGISTERED(R.string.you_are_not_registered),
+    USERS_DUPLICATION(R.string.A_user_with_this_name_already_exists),
+    EMPTY_CELLS(R.string.fill_in_all_the_fields),
+    EMPTY_LOGIN(R.string.enter_your_login),
+    REGISTER_SUCCESS(R.string.you_have_successfully_registered),
+    CORRECT_PASSWORD(R.string.correct_password),
+    INCORRECT_PASSWORD(R.string.incorrect_password),
+    YOUR_PASSWORD(R.string.your_password_is)
 }

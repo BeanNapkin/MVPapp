2da0d138c5a10fcac3ddbbc01f74bc06160ca7af
Margarita Fateeva <margarita@fateev.pro>
2022-04-10 21:23:01 +0400

    Delete interface Presenter

--- app/src/main/java/pro/fateeva/mvpapp/ui/LoginContract.kt
+++ app/src/main/java/pro/fateeva/mvpapp/ui/LoginContract.kt
@@ -9,18 +9,8 @@ class LoginContract {
         @MainThread
         fun setResponse(response: Int)
         @MainThread
-        fun setResponse(response: Int, arg: String)
-        @MainThread
         fun showProgress()
         @MainThread
         fun hideProgress()
-    }
-
-    interface Presenter {
-        fun onAttach(view: View)
-        fun checkPassword(login: String, password: String) : Response
-        fun onLogin(login: String, password: String)
-        fun onForgetPassword(password: String)
-        fun onSignUp(login: String, password: String)
     }
 }
